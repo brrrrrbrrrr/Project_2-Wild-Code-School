671104d16d7c36a76e42ca67b7ffee7742b7ec4b
Adrien Davy <adriendavyfx@gmail.com>
2023-02-08 12:15:30 +0100

    initialisation TransitionAccueil component + import logo.png in src

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TransitionAccueil from "../components/transitionAccueil/TransitionAccueil";
+
+function Home() {
+  return (
+    <div>
+      <TransitionAccueil />
+    </div>
+  );
+}
+
+export default Home;
