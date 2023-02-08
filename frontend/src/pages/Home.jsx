b463ea827be9adef115391eb34001d679448e831
Adrien Davy <adriendavyfx@gmail.com>
2023-02-08 16:37:25 +0100

    Responsive TransitionAccueil DONE

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Home.css";
 import TransitionAccueil from "../components/transitionAccueil/TransitionAccueil";
 
 function Home() {
-  return (
-    <div>
-      <TransitionAccueil />
-    </div>
-  );
+  const [loader, setLoader] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoader(false);
+    }, 5000);
+  }, []);
+
+  return loader ? <TransitionAccueil /> : <div className="Home">Home</div>;
 }
 
 export default Home;
