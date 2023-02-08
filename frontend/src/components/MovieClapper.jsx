614eaf7d731bff108ec188acd10e4e5e12f8a3d7
sdp971 <doris190485@hotmail.com>
2023-02-08 11:45:28 +0100

    created file MovieClapper.jsx

---
+++ frontend/src/components/MovieClapper.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MovieClapper() {
+  return <div />;
+}
+
+export default MovieClapper;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Coucou</h1>
+    </div>
+  );
+}
+
+export default Home;
