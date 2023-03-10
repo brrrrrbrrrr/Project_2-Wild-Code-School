1fbda77f95ebe1bc64c2d48dd6ea231ee8f760f2
brrrrrbrrrr <benjamin.chaillan-pro@laposte.net>
2023-03-10 10:38:52 +0100

    changing components footer to Footer and CSS

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./footer.css";
+import "./Footer.css";
 import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
 import logo from "../../assets/logofooter.png";
 
